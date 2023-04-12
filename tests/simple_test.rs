3e426fec6c79cc9c4a71bfaebb5ded763ebb0a69
loggar <charly.loggar@gmail.com>
2023-04-12 23:59:12 +1000

    feat (test) simple test

---
+++ tests/simple_test.rs
@@ -0,0 +1,17 @@
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// $ .../tests> cargo test --test simple_test
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 3), 5);
+        assert_eq!(add(-1, 1), 0);
+        assert_eq!(add(0, 0), 0);
+    }
+}
