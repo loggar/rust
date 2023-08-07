952b9ea4c68a5d41d8d89d4169512cf415987177
Charly LEE <charly.loggar@gmail.com>
2023-08-07 17:37:58 +1000

    refactor (test) combine files

--- tests/_t_statement_tests.rs
+++
@@ -1,8 +0,0 @@
-mod statement {
-    pub mod control_flow_if_test;
-    pub mod control_flow_in_test;
-    pub mod control_flow_loop_test;
-    pub mod control_flow_while_test;
-}
-
-// $ cargo test --test _t_statement_tests
