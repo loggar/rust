dfb92d0f3dadfa89f4a7e82a970f2ec4b1f7105c
Charly LEE <charly.loggar@gmail.com>
2023-09-14 18:39:39 +1000

    feat (error) handle error

---
+++ ex-apps/ex_error/panic_avoid_panic.rs
@@ -0,0 +1,19 @@
+fn main() {
+    loop {
+        // snip
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        if guess < 1 || guess > 100 {
+            println!("The secret number will be between 1 and 100.");
+            continue;
+        }
+
+        match guess.cmp(&secret_number) {
+            // snip
+        }
+    }
+}
