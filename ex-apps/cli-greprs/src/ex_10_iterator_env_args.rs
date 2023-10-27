dbfd0c62ec2819b48c86283d947622401d21a3c2
Charly LEE <charly.loggar@gmail.com>
2023-10-27 16:17:39 +1100

    feat iterators

---
+++ ex-apps/cli-greprs/src/ex_10_iterator_env_args.rs
@@ -0,0 +1,50 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+
+fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    println!("ignore_case", config.ignore_case);
+
+    if let Err(e) = greprs::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+}
+
+impl Config {
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            ignore_case,
+        })
+    }
+}
