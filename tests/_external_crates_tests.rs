e27637f609d8cec9710a03ad1fa6b300614c8e88
Charly LEE <charly.loggar@gmail.com>
2024-03-19 23:07:24 +1100

    refactor (dir) typename test

---
+++ tests/_external_crates_tests.rs
@@ -0,0 +1,5 @@
+mod external_crates {
+    mod crate_typename_usage_test;
+}
+
+// $ cargo test --test _external_crates_tests
--- tests/_t_type_names_tests.rs
+++
@@ -1,6 +0,0 @@
-mod type_names {
-    pub mod crate_type_name_usage_test;
-    pub mod lib_get_type_of_usage_test;
-}
-
-// $ cargo test --test _t_type_names_tests
