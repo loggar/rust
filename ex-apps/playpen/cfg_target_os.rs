2fe802dcaef903c7ab2d8fb7f9153364979e3808
Charly LEE <charly.loggar@gmail.com>
2025-01-17 14:44:11 +1100

    feat(attribute): cfg target_os

---
+++ ex-apps/playpen/cfg_target_os.rs
@@ -0,0 +1,27 @@
+// This function only gets compiled if the target OS is linux
+#[cfg(target_os = "linux")]
+fn are_you_on_linux() {
+    println!("You are running linux!");
+}
+
+// And this function only gets compiled if the target OS is *not* linux
+#[cfg(not(target_os = "linux"))]
+fn are_you_on_linux() {
+    println!("You are *not* running linux!");
+}
+
+fn main() {
+    are_you_on_linux();
+
+    println!("Are you sure?");
+    if cfg!(target_os = "linux") {
+        println!("Yes. It's definitely linux!");
+    } else {
+        println!("Yes. It's definitely *not* linux!");
+    }
+}
+
+/*
+rustc ./ex-apps/playpen/cfg_target_os.rs --out-dir ./target.ex-apps && \
+./target.ex-apps/cfg_target_os
+*/
--- ex-apps/recursive/readme.md
+++ ex-apps/recursive/readme.md
@@ -10,3 +10,9 @@ $ cargo build --release
 $ cd target/release
 $ hyperfine -i ./recursive
 ```
+
+Run:
+
+```
+cargo run --package recursive --bin recursive
+```
