ea4a3b9e9e10841a6352aaf7c9c8cf71b3621841
Charly LEE <charly.loggar@gmail.com>
2025-01-17 16:01:24 +1100

    feat(playpen): add example demonstrating variable bindings, mutability, scope, and shadowing in Rust

---
+++ ex-apps/playpen/types_variables.rs
@@ -0,0 +1,73 @@
+fn bindings() {
+    let an_integer = 1u32;
+    let a_boolean = true;
+    let unit = ();
+
+    // copy `an_integer` into `copied_integer`
+    let copied_integer = an_integer;
+
+    println!("An integer: {:?}", copied_integer);
+    println!("A boolean: {:?}", a_boolean);
+    println!("Meet the unit value: {:?}", unit);
+
+    // The compiler warns about unused variable bindings; these warnings can
+    // be silenced by prefixing the variable name with an underscore
+    let _unused_variable = 3u32;
+
+    // let noisy_unused_variable = 2u32;
+    // FIXME ^ Prefix with an underscore to suppress the warning
+    // Please note that warnings may not be shown in a browser
+}
+
+fn mutability() {
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+
+    println!("Before mutation: {}", mutable_binding);
+
+    // Ok
+    mutable_binding += 1;
+
+    println!("After mutation: {}", mutable_binding);
+
+    // Error! Cannot assign a new value to an immutable variable
+    // _immutable_binding += 1;
+}
+
+fn scope() {
+    // This binding lives in the main function
+    let long_lived_binding = 1;
+
+    // This is a block, and has a smaller scope than the main function
+    {
+        // This binding only exists in this block
+        let short_lived_binding = 2;
+
+        println!("inner short: {}", short_lived_binding);
+    }
+    // End of the block
+
+    // Error! `short_lived_binding` doesn't exist in this scope
+    // println!("outer short: {}", short_lived_binding);
+    // FIXME ^ Comment out this line
+
+    println!("outer long: {}", long_lived_binding);
+}
+
+fn shadowing() {
+    let shadowed_binding = 1;
+
+    {
+        println!("before being shadowed: {}", shadowed_binding);
+
+        // This binding *shadows* the outer one
+        let shadowed_binding = "abc";
+
+        println!("shadowed in inner block: {}", shadowed_binding);
+    }
+    println!("outside inner block: {}", shadowed_binding);
+
+    // This binding *shadows* the previous binding
+    let shadowed_binding = 2;
+    println!("shadowed in outer block: {}", shadowed_binding);
+}
